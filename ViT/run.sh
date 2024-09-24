989174e4a6f973798214433f1c11d761249d3c08
jgryu <ryu5038161@gmail.com>
2024-09-24 14:43:24 +0900

    test

--- ViT/run.sh
+++ ViT/run.sh
@@ -1,11 +1,15 @@
 # imagenet21k pre-train
-wget https://storage.googleapis.com/vit_models/imagenet21k/ViT-B_16.npz
+# wget https://storage.googleapis.com/vit_models/imagenet21k/ViT-B_16.npz
 
 # # imagenet21k pre-train + imagenet2012 fine-tuning
 # wget https://storage.googleapis.com/vit_models/imagenet21k+imagenet2012/ViT-B_16-224.npz
 
-# python train.py --name imagenet1k_TF --dataset imagenet1K \
-# --data_path /workspace/imagenet --model_type ViT-B_16 \
-# --num_steps 20000 --eval_every 1000 --train_batch_size 32 \
-# --gradient_accumulation_steps 1 --img_size 384 --learning_rate 0.06
+python train.py --name imagenet1k_TF --dataset imagenet1K \
+--data_path /workspace/imagenet --model_type ViT-B_16 \
+--num_steps 20000 --eval_every 1000 --train_batch_size 32 \
+--gradient_accumulation_steps 1 --img_size 384 --learning_rate 0.06
 
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+python -u train.py --name imagenet1k_TF --dataset imagenet1K --data_path /workspace/imagenet \
+--model_type ViT-B_16 --num_steps 20000 --eval_every 1000 --train_batch_size 64 \
+--gradient_accumulation_steps 2 --img_size 384 --learning_rate 0.06 --autocast
