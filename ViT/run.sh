67168eb5b2f1fda05863320936f89fa0067d5b80
jgryu <ryu5038161@gmail.com>
2024-12-26 18:57:54 +0900

    from server

--- ViT/run.sh
+++ ViT/run.sh
@@ -61,81 +61,64 @@
 
 # wait $!
 
-
-nohup mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 python -u train_distill.py --name B2S_maskedkd_98_scratch_2 --dataset imagenet1K --data_path /workspace/imagenet \
 --model_type ViT-S_16 --num_steps 100000 --eval_every 1000 --train_batch_size 512 \
 --gradient_accumulation_steps 2 --img_size 224 --learning_rate 5e-4 --autocast \
 --teacher_model_type ViT-B_16 --teacher_pretrained_dir output/L2B_maskedkd_98_ft_checkpoint.pth \
 --maskedkd --len_num_keep 98 \
---log_path logs/B2S_maskedkd_98_scratch_2 > running_logs/B2S_maskedkd_98_scratch_2.txt 2>&1 &
+--log_path logs/B2S_maskedkd_98_scratch_2 | tee running_logs/B2S_maskedkd_98_scratch_2.txt
 
-wait $!
-
-nohup mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 python -u train_distill.py --name B2S_naive_distill_scratch_2 --dataset imagenet1K --data_path /workspace/imagenet \
 --model_type ViT-S_16 --num_steps 100000 --eval_every 1000 --train_batch_size 512 \
 --gradient_accumulation_steps 2 --img_size 224 --learning_rate 5e-4 --autocast \
 --teacher_model_type ViT-B_16 --teacher_pretrained_dir output/L2B_maskedkd_98_ft_checkpoint.pth \
---log_path logs/B2S_naive_distill_scratch_2 > running_logs/B2S_naive_distill_scratch_2.txt 2>&1 &
+--log_path logs/B2S_naive_distill_scratch_2 | tee running_logs/B2S_naive_distill_scratch_2.txt
 
-wait $!
-
-nohup mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 python -u train.py --name S_16_scratch_2 --dataset imagenet1K --data_path /workspace/imagenet \
 --model_type ViT-S_16 --num_steps 100000 --eval_every 1000 --train_batch_size 512 \
 --gradient_accumulation_steps 2 --img_size 224 --learning_rate 5e-4 --autocast \
---log_path logs/VIT-S_16_scratch_2 > running_logs/S_16_scratch_2.txt 2>&1 &
+--log_path logs/VIT-S_16_scratch_2 | tee running_logs/S_16_scratch_2.txt
 
-wait $!
-
-nohup mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 python -u train.py --name S_16_scratch --dataset imagenet1K --data_path /workspace/imagenet \
 --model_type ViT-S_16 --num_steps 10000000 --eval_every 1000 --train_batch_size 64 \
 --gradient_accumulation_steps 2 --img_size 224 --learning_rate 5e-4 --autocast \
---log_path logs/VIT-S_16_scratch > running_logs/S_16_scratch.txt 2>&1 &
+--log_path logs/VIT-S_16_scratch | tee running_logs/S_16_scratch.txt
 
-wait $!
-
-nohup mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 python -u train_distill.py --name B2S_maskedkd_147_scratch_2 --dataset imagenet1K --data_path /workspace/imagenet \
 --model_type ViT-S_16 --num_steps 100000 --eval_every 1000 --train_batch_size 512 \
 --gradient_accumulation_steps 2 --img_size 224 --learning_rate 5e-4 --autocast \
 --teacher_model_type ViT-B_16 --teacher_pretrained_dir output/L2B_maskedkd_98_ft_checkpoint.pth \
 --maskedkd --len_num_keep 147 \
---log_path logs/B2S_maskedkd_147_scratch_2 > running_logs/B2S_maskedkd_147_scratch_2.txt 2>&1 &
+--log_path logs/B2S_maskedkd_147_scratch_2 | tee running_logs/B2S_maskedkd_147_scratch_2.txt
 
-wait $!
-
-nohup mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 python -u train_distill.py --name L2S_maskedkd_98_scratch_2 --dataset imagenet1K --data_path /workspace/imagenet \
 --model_type ViT-S_16 --num_steps 100000 --eval_every 1000 --train_batch_size 512 \
 --gradient_accumulation_steps 2 --img_size 224 --learning_rate 5e-4 --autocast \
 --teacher_model_type ViT-L_16 --teacher_pretrained_dir output/ViT-L_16_checkpoint.pth \
 --maskedkd --len_num_keep 98 \
---log_path logs/L2S_maskedkd_98_scratch_2 > running_logs/L2S_maskedkd_98_scratch_2.txt 2>&1 &
+--log_path logs/L2S_maskedkd_98_scratch_2 | tee running_logs/L2S_maskedkd_98_scratch_2.txt
 
-wait $!
-
-nohup mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 python -u train_distill.py --name L2S_naive_distill_scratch_2 --dataset imagenet1K --data_path /workspace/imagenet \
 --model_type ViT-S_16 --num_steps 100000 --eval_every 1000 --train_batch_size 512 \
 --gradient_accumulation_steps 2 --img_size 224 --learning_rate 5e-4 --autocast \
 --teacher_model_type ViT-L_16 --teacher_pretrained_dir output/ViT-L_16_checkpoint.pth \
---log_path logs/L2S_naive_distill_scratch_2 > running_logs/L2S_naive_distill_scratch_2.txt 2>&1 &
+--log_path logs/L2S_naive_distill_scratch_2 | tee running_logs/L2S_naive_distill_scratch_2.txt
 
-wait $!
-
-
-nohup mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
+mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 python -u train_distill.py --name L2S_maskedkd_147_scratch_2 --dataset imagenet1K --data_path /workspace/imagenet \
 --model_type ViT-S_16 --num_steps 100000 --eval_every 1000 --train_batch_size 512 \
 --gradient_accumulation_steps 2 --img_size 224 --learning_rate 5e-4 --autocast \
 --teacher_model_type ViT-L_16 --teacher_pretrained_dir output/ViT-L_16_checkpoint.pth \
 --maskedkd --len_num_keep 147 \
---log_path logs/L2S_maskedkd_147_scratch_2 > running_logs/L2S_maskedkd_147_scratch_2.txt 2>&1 &
+--log_path logs/L2S_maskedkd_147_scratch_2 | tee running_logs/L2S_maskedkd_147_scratch_2.txt
 
-wait $!
 
 # mpirun -n 8 --bind-to core --map-by socket:PE=6 --rank-by core --report-bindings --allow-run-as-root \
 # python -u train.py --name test --dataset imagenet1K --data_path /workspace/imagenet \
